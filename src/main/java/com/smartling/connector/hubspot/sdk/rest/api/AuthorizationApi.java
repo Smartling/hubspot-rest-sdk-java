f5ef5501fe98fbd3b2189114dedba92a46bc1920
Oleksii Burov <aburov@smartling.com>
2015-07-31 14:11:53 +0300

    HBSP-135 Added header due to Hubspot docs.

--- src/main/java/com/smartling/connector/hubspot/sdk/rest/api/AuthorizationApi.java
+++ src/main/java/com/smartling/connector/hubspot/sdk/rest/api/AuthorizationApi.java
@@ -2,12 +2,14 @@ package com.smartling.connector.hubspot.sdk.rest.api;
 
 import com.smartling.connector.hubspot.sdk.RefreshTokenData;
 import feign.Body;
+import feign.Headers;
 import feign.Param;
 import feign.RequestLine;
 
 public interface AuthorizationApi
 {
     @RequestLine("POST /auth/v1/refresh")
+    @Headers("Content-Type: application/x-www-form-urlencoded")
     @Body("client_id={client_id}&grant_type=refresh_token&refresh_token={refresh_token}")
     RefreshTokenData newToken(@Param("client_id") final String clientId, @Param("refresh_token") final String refreshToken);
 }

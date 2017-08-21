5336105dc6045d9c6519ebddd65dc103c39ebec0
Valdimir Ladynev <v.ladynev@gmail.com>
2017-08-21 17:06:09 +0300

    version 2.0.3

--- src/main/java/com/smartling/connector/hubspot/sdk/rest/token/TokenProvider.java
+++ src/main/java/com/smartling/connector/hubspot/sdk/rest/token/TokenProvider.java
@@ -5,5 +5,7 @@ import com.smartling.connector.hubspot.sdk.RefreshTokenData;
 
 public interface TokenProvider
 {
-    public RefreshTokenData getTokenData() throws HubspotApiException;
+    String TOKEN_PROVIDER_DECORATOR_CLASS = "smartling.hubspot.tokenprovider.decorator";
+
+    RefreshTokenData getTokenData() throws HubspotApiException;
 }

f3f7c75097a5054135a725b239383a42a62c2bfa
Ruslan Malona <rmalyona@smartling.com>
2018-11-21 15:42:30 +0200

    Update BlogPostDetail serialization

--- src/main/java/com/smartling/connector/hubspot/sdk/blog/BlogPostDetail.java
+++ src/main/java/com/smartling/connector/hubspot/sdk/blog/BlogPostDetail.java
@@ -1,11 +1,11 @@
 package com.smartling.connector.hubspot.sdk.blog;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.smartling.connector.hubspot.sdk.NameAware;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.Date;
-import java.util.List;
 
 @Data
 @NoArgsConstructor
@@ -14,10 +14,9 @@ public class BlogPostDetail implements NameAware
     String id;
     Long blogAuthorId;
     Long contentGroupId;
-    String featuredImage;
-    List<String> keywords;
     String name;
     String postBody;
     String postSummary;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     Date updated;
 }

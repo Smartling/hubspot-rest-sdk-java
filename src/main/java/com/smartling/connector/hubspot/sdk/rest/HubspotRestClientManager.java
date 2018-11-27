35f781f0b2b9f19ae5eb6c92ddf19c3a6d4021c8
Ruslan Malona <rmalyona@smartling.com>
2018-11-27 14:59:41 +0200

    Get rid of Redis Token Caching

--- src/main/java/com/smartling/connector/hubspot/sdk/rest/HubspotRestClientManager.java
+++ src/main/java/com/smartling/connector/hubspot/sdk/rest/HubspotRestClientManager.java
@@ -5,14 +5,10 @@ import com.smartling.connector.hubspot.sdk.HubspotClientManager;
 import com.smartling.connector.hubspot.sdk.HubspotFormClient;
 import com.smartling.connector.hubspot.sdk.HubspotPageClient;
 import com.smartling.connector.hubspot.sdk.rest.token.HubspotTokenProvider;
-import com.smartling.connector.hubspot.sdk.rest.token.RedisCachedTokenProvider;
 import com.smartling.connector.hubspot.sdk.rest.token.TokenProvider;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class HubspotRestClientManager implements HubspotClientManager
 {
-    private static final Logger LOGGER = LoggerFactory.getLogger(HubspotRestClientManager.class);
 
     private final Configuration configuration;
     private final TokenProvider tokenProvider;
@@ -44,16 +40,6 @@ public class HubspotRestClientManager implements HubspotClientManager
     @SuppressWarnings("unchecked")
     public static TokenProvider createTokenProvider(final Configuration configuration)
     {
-        TokenProvider provider = new HubspotTokenProvider(configuration);
-        try
-        {
-            provider = new RedisCachedTokenProvider(configuration, provider);
-            LOGGER.info("tokenProvider is decorated by {} class", "RedisCachedTokenProvider");
-        }
-        catch (Exception e)
-        {
-            LOGGER.error("Cannot decorate tokenProvider by {} decorator", "RedisCachedTokenProvider", e);
-        }
-        return provider;
+        return new HubspotTokenProvider(configuration);
     }
 }

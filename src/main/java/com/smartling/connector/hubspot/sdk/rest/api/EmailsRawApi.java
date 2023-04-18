6884aec2b06d99834c6dcbc3d6c7e0130d3a2d67
Murad Muradov <mmuradov@smartling.com>
2023-04-18 14:58:52 +0500

    HBSP-408: Added a comment about new endpoint for updating an AB Email

--- src/main/java/com/smartling/connector/hubspot/sdk/rest/api/EmailsRawApi.java
+++ src/main/java/com/smartling/connector/hubspot/sdk/rest/api/EmailsRawApi.java
@@ -13,6 +13,7 @@ public interface EmailsRawApi
     @Headers("Content-Type: application/json")
     String update(@Param("email_id") String emailId, String emailAsJson);
 
+    // This is a workaround specifically made for updating A/B Emails. The standard endpoint throws an error
     @RequestLine("PUT /cosemail/v1/emails/{email_id}")
     @Headers("Content-Type: application/json")
     String updateAb(@Param("email_id") String emailId, String emailAsJson);

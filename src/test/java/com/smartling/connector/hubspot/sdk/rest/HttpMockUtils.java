5e5e20e0185cddf9741af41ea7f76ee645b957d3
Khrystyna Dudina <kdudina@smartling.com>
2025-07-29 13:58:50 +0300

    HBSP-483 Update feign

--- src/test/java/com/smartling/connector/hubspot/sdk/rest/HttpMockUtils.java
+++ src/test/java/com/smartling/connector/hubspot/sdk/rest/HttpMockUtils.java
@@ -1,16 +1,16 @@
 package com.smartling.connector.hubspot.sdk.rest;
 
+import com.github.tomakehurst.wiremock.client.ResponseDefinitionBuilder;
+import com.github.tomakehurst.wiremock.matching.StringValuePattern;
+import com.github.tomakehurst.wiremock.matching.UrlPattern;
+
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static com.github.tomakehurst.wiremock.client.WireMock.containing;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlMatching;
 
-import com.github.tomakehurst.wiremock.client.ResponseDefinitionBuilder;
-import com.github.tomakehurst.wiremock.client.UrlMatchingStrategy;
-import com.github.tomakehurst.wiremock.client.ValueMatchingStrategy;
-
 public class HttpMockUtils
 {
-    public static ValueMatchingStrategy withFormParam(String key, String value)
+    public static StringValuePattern withFormParam(String key, String value)
     {
         return containing(key + "=" + value);
     }
@@ -20,12 +20,12 @@ public class HttpMockUtils
         return aResponse().withHeader("Content-Type", "application/json").withBody(json);
     }
 
-    public static UrlMatchingStrategy urlStartingWith(String path)
+    public static UrlPattern urlStartingWith(String path)
     {
         return urlMatching(path + ".*");
     }
 
-    public static UrlMatchingStrategy path(String path)
+    public static UrlPattern path(String path)
     {
         return urlMatching(path + "(\\?.+)?");
     }
